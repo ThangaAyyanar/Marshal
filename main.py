22cd615b71da97e58d1154f9d753ab8f1d298e00
Thanga Ayyanar <thangaayyanar.a@zohocorp.com>
2020-05-05 23:15:37 +0530

    Colorful status

--- main.py
+++ main.py
@@ -2,29 +2,49 @@ from rich.console import Console
 from rich.table import Column, Table
 import csv
 
-csv_file = open('progress_data.csv')
-csv_reader = csv.reader(csv_file, delimiter=',')
-line_count = 0
 
-console = Console()
-table = Table(show_header=True, header_style="bold magenta")
-
-for row in csv_reader:
-
-    # Ignore empty line
-    if len(row) == 0:
-        continue
-
-    if line_count == 0:
-
-        table.add_column(row[0], style="dim", width=12)
-        table.add_column(row[1])
-        table.add_column(row[2])
-
+def format_status(status):
+    if status == "Not yet started":
+        return f"[bold cyan]{status}[/bold cyan]"
+    elif status == "started":
+        return f"[bold green]{status}[/bold green]"
+    elif status == "stopped":
+        return f"[bold red]{status}[/bold red]"
     else:
+        return status
 
-        table.add_row(row[0], row[1], row[2])
+def print_table(csv_reader):
 
-    line_count += 1
+    line_count = 0
 
-console.print(table)
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+
+    for row in csv_reader:
+
+        # Ignore empty line
+        if len(row) == 0:
+            continue
+
+        if line_count == 0:
+
+            table.add_column(row[0], style="dim", width=12)
+            table.add_column(row[1])
+            table.add_column(row[2])
+
+        else:
+
+            table.add_row(row[0], row[1], format_status(row[2]))
+
+        line_count += 1
+
+    console.print(table)
+
+def main():
+    csv_file = open('progress_data.csv')
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    print_table(csv_reader)
+
+if __name__ == "__main__":
+    main()
+
